eb92e92ab140b7ae6f46a9196f0f82038887053a
Ralf Lang <lang@b1-systems.de>
2021-08-05 13:20:05 +0000

    No, this test is not by Jan

--- test/BasicDispatchTest.php
+++ test/BasicDispatchTest.php
@@ -5,10 +5,10 @@ use Horde\EventDispatcher\SimpleListenerProvider;
 use \PHPUnit\Framework\TestCase;
 
 /**
- * @author     Jan Schneider <jan@horde.org>
- * @license    http://www.horde.org/licenses/lgpl21 LGPL 2.1
+ * @author     Ralf Lang <lang@b1-systems.de>
+ * @license    http://www.horde.org/licenses/bsd BSD-3-Clause
  * @category   Horde
- * @package    Core
+ * @package    EventDispatcher
  * @subpackage UnitTests
  */
 class BasicDispatchTest extends TestCase
